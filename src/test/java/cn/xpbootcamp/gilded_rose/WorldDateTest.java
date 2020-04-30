66770182677de6ed9a1cf55f7ea22dac4e36b2b0
九日木彡 <nofacer@outlook.com>
2020-04-30 18:08:22 +0800

    Create WorldDate class to manage date in the world.

---
+++ src/test/java/cn/xpbootcamp/gilded_rose/WorldDateTest.java
@@ -0,0 +1,17 @@
+package cn.xpbootcamp.gilded_rose;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class WorldDateTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+}
