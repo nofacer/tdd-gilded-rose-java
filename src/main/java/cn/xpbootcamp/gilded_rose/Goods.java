93b766e02048e6d1a143c23cffc3aa5c6eb305f7
九日木彡 <nofacer@outlook.com>
2020-04-30 19:38:26 +0800

    Update Goods

--- src/main/java/cn/xpbootcamp/gilded_rose/Goods.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Goods.java
@@ -1,7 +1,5 @@
 package cn.xpbootcamp.gilded_rose;
 
-import java.text.ParseException;
-import java.util.Calendar;
 import lombok.Data;
 
 @Data
@@ -12,17 +10,17 @@ public abstract class Goods {
   private long initQuality;
   private long currentSellIn;
   private long currentQuality;
-  private Calendar initDate;
+  private String initDate;
 
-  public Goods(long initSellIn, long initQuality, String initDate) throws ParseException {
+  public Goods(long initSellIn, long initQuality, String initDate) {
     this.initSellIn = initSellIn;
     this.initQuality = initQuality;
     this.currentSellIn = initSellIn;
     this.currentQuality = initQuality;
-    this.initDate = WorldDate.convertDateFromString(initDate);
+    this.initDate = initDate;
   }
 
-  public void calSellInOfNewDay() {
+  public void calSellInOfNewDay(String dateString) {
   }
 
   public void callQualityOfNewDay() {
